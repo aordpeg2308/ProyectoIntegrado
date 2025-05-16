c9eee183d58d4274d2caa1e6fb1bf09de1ec5cc7
Alejandro Ordoñez <alejandroordonezp@gmail.com>
2025-05-16 09:31:56 +0200

    Layout Modificado

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,11 +10,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-
-   
-        @vite(['resources/css/app.css'])
     
+        @vite(['resources/css/app.css'])
+   
 </head>
 <body class="flex flex-col min-h-screen text-[#2e2d55] bg-[#a46f40] relative">
 
