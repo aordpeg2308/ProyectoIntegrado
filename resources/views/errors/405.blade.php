579038624cec21931b2dfebbf0bde6c6313a89ff
Alejandro Ordóñez Pegalajar <110174487+aordpeg2308@users.noreply.github.com>
2025-05-22 12:17:08 +0200

    Update 405.blade.php

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,10 +1,10 @@
 
-@extends('layouts.app')
+@extends('layout.app')
 
 @section('content')
-<div class="text-center py-10">
-    <h1 class="text-4xl font-bold text-red-600">405 - Método no permitido</h1>
-    <p class="mt-4 text-lg">La acción que intentaste no está permitida. Por favor, vuelve a la página anterior o al inicio.</p>
-    <a href="{{ route('home') }}" class="mt-6 inline-block bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700">Volver al inicio</a>
-</div>
+<div class="flex justify-center items-center">
+   
+        <img src="{{ asset('no-auth.png') }}" alt="Sin autorización" class="h-[700px] w-[800px]">
+        
+      
 @endsection
