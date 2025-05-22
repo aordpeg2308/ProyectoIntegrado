42aacf1d92ee05c60fb8b502d754e574ac2750b6
Alejandro Ordóñez Pegalajar <110174487+aordpeg2308@users.noreply.github.com>
2025-05-22 12:09:01 +0200

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,10 @@
 
+@extends('layout.app')
+
+@section('content')
+<div class="flex justify-center items-center">
+   
+        <img src="{{ asset('no-auth.png') }}" alt="Sin autorización" class="h-[700px] w-[800px]">
+        
+      
+@endsection
